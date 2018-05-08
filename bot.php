50d4295df0fa68ad6e8957d0cb4d54a118eab1c9
Peerawit-SC <peerawit.sc@gmail.com>
2018-05-08 18:54:46 +0700

    edit

--- bot.php
+++ bot.php
@@ -91,7 +91,7 @@ if (!is_null($events['ESP'])) {
                 case "B":
                     // $Topic = "NodeMCU1" ;
 					// getMqttfromlineMsg($Topic,"ON");
-					$replyData = new TextMessageBuilder("test");
+					$replyData = new TextMessageBuilder($events['events'][0]['replyToken']);
 					break;
                 default:
                     $replyData = new TextMessageBuilder("ERROR");
