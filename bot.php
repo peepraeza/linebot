c9b1281d62feef0f12c2284174de64ef90ef5d88
Peerawit-SC <peerawit.sc@gmail.com>
2018-05-10 14:40:35 +0700

    edit

--- bot.php
+++ bot.php
@@ -105,25 +105,6 @@ if (!is_null($events['ESP'])) {
     echo "OK2";
 }
 
-function fern(){
-  // where are we posting to?
-  $url = 'checkdata.php';
 
-  // what post fields?
-  $data = [
-     'test' => "no"
-  ];
-
-  // build the urlencoded data
-  $ch = curl_init('checkdata.php');
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json'
-  );
-
-  $result = curl_exec($ch);
-}
 
 ?>
