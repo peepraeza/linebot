6b8b35612cba67335b91f9d18f35764bb300b5e3
Peerawit-SC <peerawit.sc@gmail.com>
2018-05-10 11:47:03 +0700

    edit

--- bot.php
+++ bot.php
@@ -5,7 +5,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  require("pub.php");
  require("line.php");
- require("checkdata.php");
 // include composer autoload
 require_once './vendor/autoload.php';
  
