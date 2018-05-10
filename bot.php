75de9a818eaa27e631a54cd45a2158f1730c752b
Peerawit-SC <peerawit.sc@gmail.com>
2018-05-10 14:34:50 +0700

    edit

--- bot.php
+++ bot.php
@@ -117,7 +117,7 @@ function fern(){
   // build the urlencoded data
   $ch = curl_init('checkdata.php');
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json'
